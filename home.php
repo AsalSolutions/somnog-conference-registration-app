d101591b22ce9c6ffb28da0c865b7b1aa192398f
Amin Abdisamad <theaminux@gmail.com>
2018-02-18 18:13:07 +0300

    Inial commit

--- home.php
+++ home.php
@@ -11,7 +11,8 @@
 
 </head>
 <body>
-<nav class="navbar navbar-light bg-light">
+<!-- Header Starts -->
+<nav class="navbar sticky-top navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">
         <img src="img/somnog.png" width="150" height="50" alt="">
@@ -35,6 +36,27 @@
         </ul>
     </div >
 </nav>
-    
+<!-- Footer Starts here-->
+<nav class="navbar fixed-bottom navbar-light bg-light">
+     <div class="container">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Network</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Systems</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Conference</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Login</a>
+            </li>
+        </ul>
+    </div>
+</nav>    
 </body>
 </html>
