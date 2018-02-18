b99599f1d8a83e597b2236dd5670b97907c2d690
Amin Abdisamad <theaminux@gmail.com>
2018-02-18 18:03:50 +0300

    Updated home.php

--- home.php
+++ home.php
@@ -16,6 +16,23 @@
         <a class="navbar-brand" href="#">
         <img src="img/somnog.png" width="150" height="50" alt="">
         </a>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Network</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Systems</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Conference</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Login</a>
+            </li>
+        </ul>
     </div >
 </nav>
     
