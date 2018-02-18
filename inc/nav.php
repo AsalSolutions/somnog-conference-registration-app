1c937fe738ad76177ed39a018aee767d135890f6
Amin Abdisamad <theaminux@gmail.com>
2018-02-18 23:00:19 +0300

    Initial commit

--- inc/nav.php
+++ inc/nav.php
@@ -1,12 +1,12 @@
 <!-- Nav Starts -->
 <nav class="navbar sticky-top navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="<?php echo $_SERVER['PHP_SELF'];?>">
         <img src="img/somnog.png" width="150" height="50" alt="">
         </a>
         <ul class="nav">
             <li class="nav-item">
-                <a class="nav-link active" href="#">Home</a>
+                <a class="nav-link active" href="<?php echo $_SERVER['PHP_SELF'];?>">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Network</a>
