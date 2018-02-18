261c6b460707391079e53809f49f975a3576fbbe
Amin Abdisamad <theaminux@gmail.com>
2018-02-18 17:46:57 +0300

    Initial commit

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SomNOG Conference </title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+
+</head>
+<body>
+<nav class="navbar navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+        <img src="img/somnog.png" width="150" height="50" alt="">
+        </a>
+    </div >
+</nav>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+/**
+*Somali Network Operators’ Group
+*Implementing Network Infrastructure
+*Workshop Application Form
+*Mogadishu-Somalia (14 th - 15 th August, 2017)
+
+*Please fill in all items completely. Application forms with missing information will not be considered for this workshop
+*Full Name (As Appears on the Certificate
+*Institution name:
+*Institutional address:
+*City
+*Email address:
+*Telephone number:
+*Job title:
+*Description of duties and responsibilities
+*Have you used/administered TCP/IP networks?  If yes
+*Describe your experience (length of time versions etc)
+*Have you configured or otherwise managed any of the following:
+*Routers
+*Switches
+*Network Monitoring Systems
+*Network Operation Center
+
+*List any routing/switches courses you attended? And name the training Organization
+*Have you attended previous SomNOG activities/workshops?
+*Are you self-Sponsored? Or Sponsored by Organization? Name the organization
+
+*Email completed form to The applications close on 30th June 2017. Applications received after this date will not be considered.
+
+*Please ensure that you submit your application in time and fully completed. Selections will be on a competitive basis. Only shortlisted applicants will be contacted.
+
+
+
+
+ */
+
+
+
+
+
+
+?>
