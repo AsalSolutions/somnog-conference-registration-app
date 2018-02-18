a583513bc23c14f4bfe67d8563d05dffa35fd2bc
Amin Abdisamad <theaminux@gmail.com>
2018-02-18 18:27:24 +0300

    Initial commit

---
+++ inc/footer.php
@@ -0,0 +1,24 @@
+<!-- Footer Starts here-->
+<nav class="navbar fixed-bottom navbar-light bg-light">
+     <div class="container">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Network</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Systems</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Conference</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Login</a>
+            </li>
+        </ul>
+    </div>
+</nav>    
+</body>
+</html>
---
+++ inc/nav.php
@@ -0,0 +1,25 @@
+<!-- Nav Starts -->
+<nav class="navbar sticky-top navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+        <img src="img/somnog.png" width="150" height="50" alt="">
+        </a>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Network</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Systems</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Conference</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Login</a>
+            </li>
+        </ul>
+    </div >
+</nav>
