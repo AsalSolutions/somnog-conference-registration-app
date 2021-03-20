437516cb5b2bf1be7ef6d7645a58055b5a292899
Amin Abdisamad <theaminux@gmail.com>
2021-03-20 06:19:45 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php 
 define('ROOT_URL','http://localhost:8080/somnog/');
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','Aminux@11');
+define('DB_USER',USER);
+define('DB_PASS',PASSWORD);
 define('DB_NAME','somnog');
 
